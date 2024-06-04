b1f0c509271034c19bcf2bd03b6d1048e56ddef6
Seva <137809136+Sevatoxin@users.noreply.github.com>
2024-06-04 21:44:22 +0200

    <Setup> File structure, add game class

---
+++ lib/game.rb
@@ -0,0 +1,12 @@
+class Game
+  def initialize
+    @word = get_random_word
+  end
+
+  def get_random_word
+    words = File.readlines("words.txt")
+    word_number = Random.rand(words.length)
+    get_random_word if words[word_number].length < 5 or words[word_number].length > 12
+    words[word_number]
+  end
+end
